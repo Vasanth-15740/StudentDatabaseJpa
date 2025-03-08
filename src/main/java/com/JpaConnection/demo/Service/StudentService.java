d7d3bcb6d57843f6433c8692f716a2ea91e9eadc
vasanth <vasanth15740@gmail.com>
2025-03-08 09:51:43 +0530

    MadeChanges

--- src/main/java/com/JpaConnection/demo/Service/StudentService.java
+++ src/main/java/com/JpaConnection/demo/Service/StudentService.java
@@ -5,9 +5,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import java.util.List;
 
-
 @Service
 public class StudentService {
+
     @Autowired
     StudentInterface studentInterface;
 
